4d37bd1cc4759a5971837b8b019f227cb4eeec4d
AshutoshIIT1234 <ashutosh_2312res192@iitp.ac.in>
2025-03-19 12:10:16 +0530

    Create 1.cpp

---
+++ Basics/1.cpp
@@ -0,0 +1,21 @@
+//First C++ Program
+#include<iostream>
+using namespace std;
+int main(){
+    int a;
+    cout<<"Enter a number: ";
+    cin>>a;
+    if (a>20)
+    {
+        cout<<"Love";
+    }
+    else if(a=24)
+    {
+        cout<<"Lovely";
+    }
+    else{
+    cout<<"Babbar";
+    }
+
+    return 0;
+}
